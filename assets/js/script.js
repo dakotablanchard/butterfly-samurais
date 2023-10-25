35c2b9ecf654dd44f9d4249bfc2e03194df51294
Blake Skor <blake.skor@outlook.com>
2023-10-24 20:41:41 -0400

    added places api and autocomplete box

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function initPlacesAutocomplete() {
+	const placesAutocompleteBox = new google.maps.places.Autocomplete(
+		document.getElementById("places-search-box"),
+		{
+			fields: ["address_components", "geometry", "icon", "name"],
+		}
+	);
+}
