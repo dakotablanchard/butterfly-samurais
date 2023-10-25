eb78fe468fc9e140282fbcf116a34692dd8b2cb5
Blake Skor <blake.skor@outlook.com>
2023-10-24 21:50:52 -0400

    added openai call with fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-const OPEN_AI_KEY = "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";
+const OPENAI_API_KEY = "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";
 
 const AI_USER_STORY = "You are a travel planner.";
 
@@ -12,12 +12,49 @@ function initPlacesAutocomplete() {
 }
 
 function openAiCall(prompt) {
-	return prompt;
+	const data = {
+		model: "gpt-3.5-turbo",
+		messages: [
+			{
+				role: "system",
+				content: "You are a helpful assistant.",
+			},
+			{
+				role: "user",
+				content: prompt,
+			},
+		],
+	};
+
+	return fetch("https://api.openai.com/v1/chat/completions", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			Authorization: `Bearer ${OPENAI_API_KEY}`,
+		},
+		body: JSON.stringify(data),
+	})
+		.then((response) => response.json())
+		.then((data) => {
+			return data.choices[0].message.content;
+		})
+		.catch((error) => {
+			console.error("Error:", error);
+		});
 }
 
 function searchSubmission(event) {
 	event.preventDefault();
-	document.getElementById("test-response").textContent = openAiCall("test");
+	let activityBox = document.getElementById("activity-search-box");
+	let prompt = activityBox.value;
+	activityBox.value = "";
+	openAiCall(prompt)
+		.then((response) => {
+			document.getElementById("test-response").textContent = response;
+		})
+		.catch((error) => {
+			console.error("Error:", error);
+		});
 }
 
 function init() {
