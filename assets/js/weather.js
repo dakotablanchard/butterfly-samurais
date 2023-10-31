c8059d914214f8aa12672059b132d3ae11fd2575
Dakota Blanchard <dakotablanchard2699@gmail.com>
2023-10-30 21:17:43 -0400

    working on weather api

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,21 +1,35 @@
 // Pull date and location from welcome page
 // If date is within 16 days of current date then make api call to open weather
 // Else return string "Check back within 16 days of your trip to see weather forecast"
+var openweatherApiKey = "b3b885146dce3d4c2a9d8e921432d8fb"
 
-const searchForm2 = document.getElementById("search-form");
-const locationBox2 = document.getElementById("places-search-box");
-const datepicker2 = document.getElementById("datepicker");
+var searchForm2 = document.getElementById("search-form");
+var locationBox2 = document.getElementById("places-search-box");
+var datepicker2 = document.getElementById("datepicker");
+
+var locationValue = locationBox2.value
+var dateValue = datepicker2.value
 
 function getWeather(event) {
     event.preventDefault()
-    console.log(locationBox2.value)
-    console.log(datepicker2.value)
+    
+    // console.log(locationValue)
+    var weatherURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + locationBox2.value + "&appid=b3b885146dce3d4c2a9d8e921432d8fb"
+
+    fetch(weatherURL)
+        .then(function (response) {
+            response.json();
+        })
+        .then(function (data) {
+            console.log(data)
+            return data
+        });
 }
 
 //run on page load
 function init() {
-	//add on submit event to form
-	searchForm2.addEventListener("submit", getWeather);
+    //add on submit event to form
+    searchForm2.addEventListener("submit", getWeather);
 }
 
 //run the on page load function
