9600c4c18afd73fbe9c72375598967676d19aaff
mighty-little-coder <144641610+mighty-little-coder@users.noreply.github.com>
2023-11-05 00:29:50 -0400

    Added current weather display card functionality. need to iron out details

--- assets/js/results.js
+++ assets/js/results.js
@@ -1,3 +1,6 @@
+// Weather Api inputs
+var APIKey = "3ac4c533f75c393e9ad9feff434508cf";
+var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + APIKey;
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const uuid = urlParams.get("uuid");
@@ -10,9 +13,11 @@ const auxData = jsonResponseArr[1];
 const dateBox = document.getElementById("datepicker");
 
 const activityWrapper = document.getElementById("activity-wrapper");
+const weatherWrapper = document.getElementById("weather-wrapper");
 
 function init() {
 	displayResponse();
+	displayWeather();
 }
 
 function displayResponse() {
@@ -49,56 +54,56 @@ function createEventCard(eventObj) {
 }
 
 
+// function displayWeather() {
+// 	for (location of auxData) {
+// 		createEventCard(weather);
+// 	}
 
+
+// Must adress the "THEN" loop.
+
+// 	fetch(queryURL).then(function (response) {
+// 		console.log(response);
+// 		return response.json();
+// 	})
+
+// 	.then(function (weatherData) {
+// 		renderCurrentDest(weatherData);
+// 	});
+// }
 
 
 function createWeatherCard(weatherObj) {
-	var locationWeather = auxData.location;
+	let header = weatherObj.heading;
+	let description = weatherObj.description;
+	var locationWeather = auxData.location; // need to set this var using weatherAPI. not location...
 	console.log(locationWeather);
 
-// 	let header = weatherObj.heading;
-// 	let description = weatherObj.description;
+	const colDiv = document.createElement("div");
+	colDiv.className = "col s12";
 
-// 	const colDiv = document.createElement("div");
-// 	colDiv.className = "col s12";
+	const cardDiv = document.createElement("div");
+	cardDiv.className = "card";
+	colDiv.appendChild(cardDiv);
 
-// 	const cardDiv = document.createElement("div");
-// 	cardDiv.className = "card";
-// 	colDiv.appendChild(cardDiv);
+	const cardTitleSpan = document.createElement("span");
+	cardTitleSpan.className = "card-title";
+	cardTitleSpan.textContent = header;
+	cardDiv.appendChild(cardTitleSpan);
 
-// 	const cardTitleSpan = document.createElement("span");
-// 	cardTitleSpan.className = "card-title";
-// 	cardTitleSpan.textContent = header;
-// 	cardDiv.appendChild(cardTitleSpan);
+	const cardContentDiv = document.createElement("div");
+	cardContentDiv.className = "card-content";
+	cardDiv.appendChild(cardContentDiv);
 
-// 	const cardContentDiv = document.createElement("div");
-// 	cardContentDiv.className = "card-content";
-// 	cardDiv.appendChild(cardContentDiv);
+	const paragraph = document.createElement("p");
+	paragraph.textContent = description;
+	cardContentDiv.appendChild(paragraph);
 
-// 	const paragraph = document.createElement("p");
-// 	paragraph.textContent = description;
-// 	cardContentDiv.appendChild(paragraph);
-
-// 	activityWrapper.appendChild(colDiv);
+	weatherWrapper.appendChild(colDiv);
 }
 
-
-
-// // Function to display current weather at chosen destination
-// var APIKey = "3ac4c533f75c393e9ad9feff434508cf";
-// var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + APIKey;
-
-// function displayForecast(currentWeatherData) {
-
-// 	// 	//Need to add ability to find selected date!!!
-// 	document.querySelector(".cityName").innerHTML = currentWeatherData.city.name;
-// 	document.querySelector("#todayTemp").innerHTML = "Temperature: " + currentWeatherData.list[0].main.temp + "°F";
-// 	document.querySelector("#todayWind").innerHTML = "Wind Speed: " + currentWeatherData.list[0].wind.speed + "mph";
-// 	document.querySelector("#todayHumid").innerHTML = "Humidity: " + currentWeatherData.list[0].main.humidity + "%";
-// }
-
 console.log(auxData.location);
-// console.log(auxData.date);
+console.log(auxData.date);
 
 
 init();
